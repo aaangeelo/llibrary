68c45de1c206fecbbf87793b55ce6d1784aecf30
Angelo Curio <iamanauthor9@gmail.com>
2022-03-10 00:04:12 +0800

    Working commit button

--- index.js
+++ index.js
@@ -2,63 +2,66 @@ const buttonAdd = document.getElementById("button-add");
 const popupClose = document.getElementById("popup__close");
 const popup = document.getElementById("popup");
 
-const categories = document.querySelectorAll(".category");
-const categoryTitle = document.querySelector(".category-title");
-const categoryAuthor = document.querySelector(".category-author");
-const categoryPages = document.querySelector(".category-pages");
-const categoryStatus = document.querySelector(".category-status");
+const wrapperMain = document.querySelector(".wrapper-main");
 
 const formItems = document.querySelectorAll(".form__item");
 const formSubmit = document.getElementById("button-submit");
 
+let editBooks = document.querySelectorAll(".button-delete");
+
+let bookID = 0;
+
 let myLibrary = [];
 
-function Book(title, author, pages, status) {
+function Book(title, author, pages, status, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.status = status;
+  this.id = id;
 }
 
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`;
+Book.prototype.displayBook = function () {
+  const book = document.createElement("div");
+  book.classList.add("book");
+  book.id = bookID;
+  wrapperMain.appendChild(book);
+
+  const bookTitle = document.createElement("div");
+  bookTitle.classList.add("book__info");
+  bookTitle.classList.add("book__info-title");
+  bookTitle.innerHTML = this.title;
+  book.appendChild(bookTitle);
+
+  const bookAuthor = document.createElement("div");
+  bookAuthor.classList.add("book__info");
+  bookAuthor.classList.add("book__info-author");
+  bookAuthor.innerHTML = `by ${this.author}`;
+  book.appendChild(bookAuthor);
+
+  const bookPages = document.createElement("div");
+  bookPages.classList.add("book__info");
+  bookPages.classList.add("book__info-pages");
+  bookPages.innerHTML = `Pages: ${this.pages}`;
+  book.appendChild(bookPages);
+
+  const buttonStatus = document.createElement("button");
+  buttonStatus.classList.add("btn");
+  buttonStatus.classList.add("button-status");
+  buttonStatus.innerHTML = this.status;
+  book.appendChild(buttonStatus);
+
+  const buttonDelete = document.createElement("button");
+  buttonDelete.classList.add("btn");
+  buttonDelete.classList.add("button-delete");
+  buttonDelete.innerHTML = "Delete";
+  book.appendChild(buttonDelete);
 };
 
-Book.prototype.displayBook = function () {
-  const titleBlock = document.createElement("div");
-  titleBlock.classList.add("title-block");
-  categoryTitle.appendChild(titleBlock);
-  // delete
-  const deleteElement = document.createElement("div");
-  deleteElement.classList.add("button");
-  deleteElement.classList.add("button-delete");
-  deleteElement.innerHTML = "&times";
-  titleBlock.appendChild(deleteElement);
-  // title
-  const titleElement = document.createElement("div");
-  titleElement.classList.add("book-info");
-  titleElement.classList.add("book-info-title");
-  titleElement.innerHTML = this.title;
-  titleBlock.appendChild(titleElement);
-  // author
-  const authorElement = document.createElement("div");
-  authorElement.classList.add("book-info");
-  authorElement.classList.add("book-info-author");
-  authorElement.innerHTML = this.author;
-  categoryAuthor.appendChild(authorElement);
-  // pages
-  const pagesElement = document.createElement("div");
-  pagesElement.classList.add("book-info");
-  pagesElement.classList.add("book-info-pages");
-  pagesElement.innerHTML = this.pages;
-  categoryPages.appendChild(pagesElement);
-  // status
-  const statusElement = document.createElement("button");
-  statusElement.classList.add("book-info");
-  statusElement.classList.add("book-info-status");
-  statusElement.innerHTML = this.status;
-  categoryStatus.appendChild(statusElement);
-};
+function showBook() {
+  for (book of myLibrary) {
+  }
+}
 
 function validateInput() {
   formItems.forEach((item) => {
@@ -80,16 +83,45 @@ function addBookToLibrary() {
   const author = document.getElementById("form__item-author").value;
   const pages = document.getElementById("form__item-pages").value;
   const status = document.querySelector('input[name="status"]:checked').value;
+  const id = bookID;
 
-  const newBook = new Book(title, author, pages, status);
+  const newBook = new Book(title, author, pages, status, id);
   newBook.displayBook();
   myLibrary.push(newBook);
 
   popup.style.display = "none";
   formItems.forEach((item) => (item.value = ""));
+  bookID++;
+  console.log(bookID);
 }
 
-popupClose.addEventListener("click", () => (popup.style.display = "none"));
-buttonAdd.addEventListener("click", () => (popup.style.display = "flex"));
+function editBook(e) {
+  for (className of e.target.classList) {
+    if (className === "button-delete") {
+      const book = e.target.parentNode;
 
-formSubmit.addEventListener("click", addBookToLibrary);
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].id == book.id) {
+          myLibrary.splice(i, 1);
+          break;
+        }
+      }
+
+      while (book.hasChildNodes()) {
+        book.removeChild(book.firstChild);
+      }
+      book.remove();
+    } else if (className === "button-status") {
+      console.log(e);
+    }
+  }
+}
+
+window.onload = function () {
+  popupClose.addEventListener("click", () => (popup.style.display = "none"));
+  buttonAdd.addEventListener("click", () => (popup.style.display = "flex"));
+
+  formSubmit.addEventListener("click", addBookToLibrary);
+
+  document.body.addEventListener("click", editBook);
+};
